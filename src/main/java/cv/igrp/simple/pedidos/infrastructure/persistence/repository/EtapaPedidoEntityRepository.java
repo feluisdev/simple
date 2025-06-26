12d6332917209628d056dcf2e66253638a13c304
feluisdev <feluis.dev@gmail.com>
2025-06-26 22:09:29 -0100

    remove expose entities in controller

--- src/main/java/cv/igrp/simple/pedidos/infrastructure/persistence/repository/EtapaPedidoEntityRepository.java
+++ src/main/java/cv/igrp/simple/pedidos/infrastructure/persistence/repository/EtapaPedidoEntityRepository.java
@@ -1,17 +1,13 @@
 package cv.igrp.simple.pedidos.infrastructure.persistence.repository;
 
 import cv.igrp.simple.pedidos.infrastructure.persistence.entity.EtapaPedidoEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
-import java.util.Optional;
+import org.springframework.stereotype.Repository;
 
 
 
 @Repository
-@RepositoryRestResource(path = "etapaPedidoEntity")
 public interface EtapaPedidoEntityRepository extends
     JpaRepository<EtapaPedidoEntity, Integer>,
     JpaSpecificationExecutor<EtapaPedidoEntity>
