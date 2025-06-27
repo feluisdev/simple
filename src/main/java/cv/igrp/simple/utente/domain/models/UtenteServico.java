c06fb02a2f45fe81a02a14afba8504061bf19a58
tchspprtcv <tchspprtcv@gmail.com>
2025-06-27 14:44:13 -0100

    repondo versão

---
+++ src/main/java/cv/igrp/simple/utente/domain/models/UtenteServico.java
@@ -0,0 +1,67 @@
+package cv.igrp.simple.utente.domain.models;
+
+import cv.igrp.simple.shared.config.AuditEntity;
+import cv.igrp.framework.stereotype.IgrpEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.envers.Audited;
+import java.time.LocalDate;
+import java.math.BigDecimal;
+import cv.igrp.simple.utente.application.constants.Estado;
+import java.util.List;
+
+@Audited
+@Getter
+@Setter
+@ToString
+@IgrpEntity
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cm_utente_objeto")
+public class UtenteServico extends AuditEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true, nullable = false)
+    private Integer id;
+
+  
+    @Column(name="objeto_tipo")
+    private String objetoTipo;
+
+  
+    @Column(name="objeto_id")
+    private Integer objetoId;
+
+  
+    @Column(name="descricao")
+    private String descricao;
+
+  
+    @Column(name="referencia")
+    private String referencia;
+
+  
+    @Column(name="datainicio")
+    private LocalDate dataInicio;
+
+  
+    @Column(name="datafim")
+    private LocalDate dataFim;
+
+  
+    @Column(name="valor")
+    private BigDecimal valor;
+
+  
+    @Enumerated(EnumType.STRING)
+    @Column(name="estado")
+    private Estado estado;
+
+     @ManyToOne(fetch = FetchType.LAZY)
+   @JoinColumn(name = "utente_id")
+   private Utente utenteId;
+
+
+}
