70c1c1bd087d8ed1e3d606bd98ca0af3d4badda4
feluisdev <feluis.dev@gmail.com>
2025-07-27 09:45:13 -0100

    chore(config): update application properties and DTO validation
    - Enable `server.port` property in application-development.properties.
    - Add `springdoc.swagger-ui.tryItOutEnabled` and `springdoc.swagger-ui.docExpansion`.
    - Enhance `CriarUtenteDTOValidator` to validate `genero` field for `CIDADAO` type.
    - Add autogenerated annotation to `CriarUtenteDTO` and standardize field formatting.

--- src/main/java/cv/igrp/simple/utente/application/dto/CriarUtenteDTO.java
+++ src/main/java/cv/igrp/simple/utente/application/dto/CriarUtenteDTO.java
@@ -1,3 +1,6 @@
+/* THIS FILE WAS GENERATED AUTOMATICALLY BY iGRP STUDIO. */
+/* DO NOT MODIFY IT BECAUSE IT COULD BE REWRITTEN AT ANY TIME. */
+
 package cv.igrp.simple.utente.application.dto;
 
 import cv.igrp.framework.stereotype.IgrpDTO;
@@ -11,62 +14,64 @@ import cv.igrp.simple.utente.application.constants.GeneroTipo;
 import cv.igrp.simple.utente.application.constants.TipoIdentificacao;
 import cv.igrp.simple.utente.application.constants.TipoUtente;
 import java.time.LocalDate;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
 @ICriarUtenteDTOValidator
+
 @IgrpDTO
 public class CriarUtenteDTO {
 
   @NotNull(message = "The field <tipoUtente> is required.")
   
-  private TipoUtente tipoUtente;
+  private TipoUtente tipoUtente ;
   @NotBlank(message = "The field <nome> is required.")
 	@Size(min = 3, message = "The field length <nome> must be at least 3 characters.")
   
-  private String nome;
+  private String nome ;
   @NotBlank(message = "The field <nif> is required.")
   
-  private String nif;
+  private String nif ;
   @NotNull(message = "The field <tipoIdentificacao> is required.")
   
-  private TipoIdentificacao tipoIdentificacao;
+  private TipoIdentificacao tipoIdentificacao ;
   @NotBlank(message = "The field <identificacao> is required.")
   
-  private String identificacao;
+  private String identificacao ;
   
   
-  private String nomeMae;
+  private String nomeMae ;
   
   
-  private String nomePai;
+  private String nomePai ;
   
   
-  private LocalDate dataNascimento;
+  private LocalDate dataNascimento ;
   @NotBlank(message = "The field <telefone> is required.")
   
-  private String telefone;
+  private String telefone ;
   @NotBlank(message = "The field <endereco> is required.")
   
-  private String endereco;
+  private String endereco ;
   @Email(message = "Invalid email format for field <email>.")
   
-  private String email;
+  private String email ;
   
   
-  private String caixaPostal;
+  private String caixaPostal ;
   
   
-  private String telemovel;
+  private String telemovel ;
   
   
-  private String departamentoResponsavel;
+  private String departamentoResponsavel ;
   
   
-  private GeneroTipo genero;
+  private GeneroTipo genero ;
   
   
-  private String nacionalidade;
+  private String nacionalidade ;
 
 }
